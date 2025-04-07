7a9fb0daac6e10737ccb001532e8827d16f90e12
4Class_08 <4Class_08@DESKTOP-2SPUH54>
2025-04-07 16:50:50 +0900

    로그인 기능 추가

---
+++ CampingWeb/src/main/java/com/camping/action/CustomerLoginOkAction.java
@@ -0,0 +1,43 @@
+package com.camping.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.camping.model.CustomerDAO;
+
+public class CustomerLoginOkAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		CustomerDAO dao = CustomerDAO.getInstance();
+		
+		int result = dao.customerLogin(id, pwd);
+		
+		 if (result == 1) {
+	            // 로그인 성공
+	            HttpSession session = request.getSession();
+	            session.setAttribute("cus_id", id);
+	            ActionForward forward = new ActionForward();
+	            forward.setRedirect(true);
+	            forward.setPath("main.jsp");
+	            return forward;
+		 
+		 } else {
+	            // 로그인 실패
+	            request.setAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
+	            ActionForward forward = new ActionForward();
+	            forward.setRedirect(false);
+	            forward.setPath("view/customer_login.jsp");
+	            return forward;
+	        }
+
+	}
+
+}
